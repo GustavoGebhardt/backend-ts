b5577cdcf8c5200ca1a48407aad487ab97a947c8
Gustavo Gebhardt <gustavogebhardt@MacBook-Air-de-Gustavo.local>
2024-07-11 10:25:44 -0300

    Dotenv

--- db/conection.ts
+++ db/conection.ts
@@ -1,9 +1,10 @@
-import mongoose from 'mongoose'
+import mongoose, { Schema } from 'mongoose'
+require('dotenv').config()
 
 export default async function conection() {
     try{
         mongoose.set("strictQuery", true)
-        await mongoose.connect("mongodb://root:Gustavo131328!@localhost:27017")
+        await mongoose.connect("mongodb://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@" + process.env.DB_HOST)
         console.log("Banco de Dados conectado!")
 
     } catch(error) {
